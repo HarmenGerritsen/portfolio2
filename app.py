11370a8f339dda82012763bf338f96b23a47b993
Harmen <harmen.gerritsen@student.hu.nl>
2020-09-24 15:25:01 +0200

    commit 6 better frontpage, contactpage is being added

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template("main.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('page-not-found.html'), 404
+
+
+if __name__ == '__main__':
+    app.run()
